4c822da70da6bff2d4e21f9c9c4b4938cfc865a3
provash10 <provash20cb@gmail.com>
2025-07-31 10:04:25 +0600

    26.1 js review class data type & operator

---
+++ index.js
@@ -0,0 +1,9 @@
+ let totalDonation = 5000000;
+        let addDonation   = 2000000;
+        let totalAmount = (totalDonation + addDonation );
+
+        let givenMoney = 90;
+
+        let eachFamilyGet = (totalAmount / givenMoney)
+
+        console.log(eachFamilyGet);
